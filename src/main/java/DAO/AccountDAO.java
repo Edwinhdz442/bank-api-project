e37b818c1bd261ee508504f22270c0f345e06d8a
Edwin <edwin.hdz442@gmail.com>
2023-03-07 16:19:45 -0500

    update DAO and minor spelling error

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -5,43 +5,55 @@ import Util.ConnectionUtil;
 import java.sql.*;
 
 public class AccountDAO {
-    public static Account addAccount(Account account)
-    {
+    public Account insertAccount(Account account) {
         Connection conn = ConnectionUtil.getConnection();
-        String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+        try{
+            String sql = "Insert INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, account.getUsername());
+            statement.setString(2, account.getPassword());
+
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account userAccount(String username, String password) {
+        Connection conn = ConnectionUtil.getConnection();
+        String sql = "SELECT * FROM account WHERE username=? AND password=?;";
         try {
-            PreparedStatement statement = conn.prepareStatement(
-                sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, account.username);
-            statement.setString(2, account.password);
-            statement.executeUpdate();
-            ResultSet result = statement.getGeneratedKeys();
-            if (result.next()) {
-                account.account_id = (int)result.getLong(1);
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getInt("account_num"),
+                        rs.getString("username"),
+                        rs.getString("password"));
                 return account;
             }
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
-    public static Account useAccount(Account account)
-    { 
+    public void deleteAccount(int account_num) {
         Connection conn = ConnectionUtil.getConnection();
-        String sql = "SELECT * FROM account WHERE username=? AND password=?;";
-        try {
+        try{
+            String sql = "DELETE FROM message WHERE account_num = ?";
             PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setString(1, account.username);
-            statement.setString(2, account.password);
-            ResultSet result = statement.executeQuery();
-            if (result.next()) {
-                account.account_id = (int)result.getLong(1);
-                return account;
-            }
-        } catch (SQLException e) {
+
+            statement.setInt(1, account_num);
+
+            statement.executeUpdate();
+        } catch (SQLException e){
             System.out.println(e.getMessage());
         }
-        return null;
     }
+
 }
