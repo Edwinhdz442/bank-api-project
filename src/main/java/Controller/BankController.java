a44aca8ccd4257d76b447cc2e26b1efe52ded53f
alex <ackerander@gmail.com>
2023-03-07 13:11:12 -0800

    Added account deletion in the controller

--- src/main/java/Controller/BankController.java
+++ src/main/java/Controller/BankController.java
@@ -15,27 +15,18 @@ import io.javalin.http.Context;
 import Model.Account;
 import Service.AccountService;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class BankController {
     private ObjectMapper mapper;
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
     public Javalin startAPI() {
         mapper = new ObjectMapper();
         Javalin app = Javalin.create();
-        app.post("/register", this::registerHandler);
-        app.post("/login", this::loginHandler);
+        app.post("/register", this::accountRegister);
+        app.delete("/account/{id}", this::accountDelete);
+        app.post("/login", this::accountLogin);
         return app;
     }
 
-    private void registerHandler(Context ctx) throws JsonProcessingException
+    private void accountRegister(Context ctx) throws JsonProcessingException
     {
         Account account = mapper.readValue(ctx.body(), Account.class);
         Account newAccount = AccountService.addAccount(account);
@@ -46,8 +37,15 @@ public class BankController {
             ctx.status(200);
         }
     }
+    
+    private void accountDelete(Context ctx) {
+        Account account = AccountService.rmAccount(Integer.parseInt(ctx.pathParam("id")));
+        if (account != null)
+            ctx.json(account);
+        ctx.status(200);
+    }
 
-    private void loginHandler(Context ctx) throws JsonProcessingException
+    private void accountLogin(Context ctx) throws JsonProcessingException
     {
         Account account = mapper.readValue(ctx.body(), Account.class);
         Account user = AccountService.useAccount(account);
