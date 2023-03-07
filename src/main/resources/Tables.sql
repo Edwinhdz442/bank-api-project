a4c82eb22194e4452574ab3c30516a237393841c
Edwin <edwin.hdz442@gmail.com>
2023-03-07 17:28:42 -0500

    sql account table added

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,3 +1,8 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
+create table account (
+account_num int primary key,
+username varchar(255) unique,
+password varchar(255)
+);
